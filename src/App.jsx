d46bdb691c78b4504a8fadca6772740f97fe5026
Francisco Oliva <franoliva845@gmail.com>
2023-09-05 21:42:59 -0300

    Entrega 1.3

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import NavBar from './components/NavBar/NavBar'
 function App() {
   return (
     <>
-      <NavBar />
+      <NavBar/>
       <ItemListContainer greeting= "Bienvenidos a BANIRA"/>
     </>
   )
