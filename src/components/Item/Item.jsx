2042e28030c8b98378335ed44843c6ef52f66371
Francisco Oliva <franoliva845@gmail.com>
2023-11-17 12:28:13 -0300

    Correcciones 1.2

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -6,6 +6,8 @@ import { db } from "../../db/db"
 
 const Item = () => {
     const [products, setProducts] = useState([])
+    const [selectedCategory, setSelectedCategory] = useState("all")
+    
     useEffect(() => {
         const productsRef = collection(db, "products")
         getDocs(productsRef).then((response) => {
@@ -14,16 +16,31 @@ const Item = () => {
         })
     }, [])
 
+    const filterByCategory = (category) => {
+        setSelectedCategory(category)
+    }
+
+    const filteredProducts = selectedCategory === "all" ? products : products.filter((prod) => prod.category === selectedCategory)
+
     return (
-        products.map(prod => (
-            <div className="itemsContenedor">
-                <img src={prod.img} alt="Error en la carga de la imágen"/>
+    <div className="items">
+        <div className="filtros">
+            <p onClick={() => filterByCategory("all")}>TODO</p>
+            <p onClick={() => filterByCategory("pants")}>PANTALONES</p>
+            <p onClick={() => filterByCategory("hoodies")}>HOODIES</p>
+            <p onClick={() => filterByCategory("tshirts")}>REMERAS</p>
+        </div>
+        <div className="itemsGroup">
+            {filteredProducts.map((prod) => (
+            <div key={prod.id} className="itemsContenedor">
+                <img src={prod.img} alt="Error en la carga de la imágen" />
                 <h2>{prod.name}</h2>
                 <p>${prod.price}</p>
                 <Link to={`/item/${prod.id}`}>VER MÁS</Link>
-            </div>
-        ))
-    )
-}
+            </div>))}
+        </div>
+    </div>
+);
+};
 
-export default Item
+export default Item;
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,24 +2,17 @@ import Layout from "../Layout/Layout"
 import Item from "../Item/Item"
 import "./ItemListContainer.css"
 import { useEffect, useState } from "react"
-import { collection, getDocs } from "firebase/firestore"
-import { db } from "../../db/db"
 
 const ItemListContainer = () => {
     const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
-        // const productsRef = collection(db, "products")
-        // getDocs(productsRef).then((response) => {
-        //     const productsFirebase = response.docs.map((product) => ( { id: product.id, ...product.data()} ))
-        //     console.log(productsFirebase)
-        // })
-
 
         setTimeout(() => {
             setIsLoading(false)
         }, 1200)
     }, [])
+
     return(
         <Layout>
             {isLoading ? (
